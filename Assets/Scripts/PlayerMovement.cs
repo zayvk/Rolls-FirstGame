cc86014b0ee0852107b0e7f1f38f4f1a832d6e45
ZayVK <jay.v.kamble@gmail.com>
2020-08-27 11:29:55 +0530

    Full Commit for Rolls Project
    Made first game in unity called Rolls. Just as small game for starter and learning unity. Committing the project to git

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    //This is a reference to the rigidbody component named rigidbody
+    public Rigidbody rigidbody;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // We marked it as FixedUpdate because we are going to mess with the physics...
+    void FixedUpdate () {
+        //Add a forword force
+        rigidbody.AddForce(0, 100, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rigidbody.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rigidbody.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rigidbody.position.x > 8f || rigidbody.position.x < -8f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+} 
